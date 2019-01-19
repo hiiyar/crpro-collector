a7df4aee6ab4ed2b73da45e56ee6c1b528501d63
Lucas Reis <thereis@live.com>
2019-01-19 13:35:50 -0200

    Added /clan/:tag route

--- src/controllers/clan.ts
+++ src/controllers/clan.ts
@@ -1,9 +1,13 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
 
 import { CRService } from "../services/cr.service";
 
 import { validateTagUtils } from "../utils/validateTag.utils";
+import { ClanSchema } from "../models/ClanSchema";
 import { Clan } from "../models/clan/Clan";
+
+const clanModel = mongoose.model("Clan", ClanSchema);
 
 export class ClanController {
   async fetchClan(
@@ -17,9 +21,24 @@ export class ClanController {
 
       let clan: Clan = response.data;
 
-      console.log(clan.memberList);
+      const schema = new clanModel({
+        _id: tag,
+        ...clan
+      });
 
-      return res.json(tag);
+      // Create a document if not exists or update it
+      clanModel.collection.findOneAndUpdate(
+        {
+          _id: tag
+        },
+        schema,
+        { upsert: true },
+        (err, raw) => {
+          if (err) throw err;
+        }
+      );
+
+      return res.json(schema.toJSON());
     } catch (e) {
       next(e);
     }
